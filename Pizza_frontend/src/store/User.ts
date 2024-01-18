72b499248768fdf656a967bd7ca4bce4a0f4514e
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2024-01-18 22:55:07 +0900

    Feat: user 저장소 추가

---
+++ Pizza_frontend/src/store/User.ts
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface UserState {
+  id: number | null;
+  email: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  isAdmin: boolean | null;
+  nickName: string | null;
+  isLogin: boolean;
+  introduction?: string | null;
+}
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    id: null,
+    email: null,
+    firstName: null,
+    lastName: null,
+    isAdmin: null,
+    nickName: null,
+    isLogin: false,
+  } as UserState,
+  reducers: {
+    setUser: (state, action) => {
+      const {
+        id,
+        email,
+        firstName,
+        lastName,
+        isAdmin,
+        nickName,
+        introduction,
+      } = action.payload;
+      state.id = id;
+      state.email = email;
+      state.firstName = firstName;
+      state.lastName = lastName;
+      state.isAdmin = isAdmin;
+      state.nickName = nickName;
+      state.introduction = introduction;
+      state.isLogin = true;
+    },
+    clearUser: (state) => {
+      state.id = null;
+      state.email = null;
+      state.firstName = null;
+      state.lastName = null;
+      state.isAdmin = null;
+      state.nickName = null;
+      state.introduction = null;
+      state.isLogin = false;
+    },
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+
+export default userSlice.reducer;
