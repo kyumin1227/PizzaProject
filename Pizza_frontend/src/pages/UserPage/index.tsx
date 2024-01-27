84b7e99c495d74d7869dd3c39ed5cc5772555a22
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2024-01-27 22:47:38 +0900

    Feat: userPage에 user 정보 수정을 위한 입력 란 추가

--- Pizza_frontend/src/pages/UserPage/index.tsx
+++ Pizza_frontend/src/pages/UserPage/index.tsx
@@ -3,13 +3,30 @@ import store from "../../store";
 import { clearUser } from "../../store/User";
 import { removeRefreshToken } from "../../store/Cookie";
 import { deleteAccessToken } from "../../store/Auth";
+import { Box, Button, Divider, TextField, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const UserPage = () => {
   const { pathname } = useLocation();
-  console.log(pathname);
   const userId = pathname.slice(6);
 
   const navigate = useNavigate();
+
+  const userInfo = useSelector((state) => {
+    return state.user;
+  });
+
+  const [amendUserId, setAmendUserId] = useState(userInfo.id);
+  const [amendUserFirstName, setAmendUserFirstName] = useState(
+    userInfo.firstName
+  );
+  const [amendUserLastName, setAmendUserLastName] = useState(userInfo.lastName);
+  const [amendUserNickName, setAmendUserNickName] = useState(userInfo.nickName);
+  const [amendUserEmail, setAmendUserEmail] = useState(userInfo.email);
+  const [amendUserIntroduction, setAmendUserIntroduction] = useState(
+    userInfo.introduction
+  );
 
   const handleLogOut = () => {
     store.dispatch(clearUser());
@@ -21,9 +38,79 @@ const UserPage = () => {
 
   return (
     <>
-      userPage {userId}
-      <br />
+      <Typography variant="h3">User Page</Typography>
       <button onClick={handleLogOut}>logout</button>
+      <Box sx={{ mx: "5%" }}>
+        <TextField
+          id="userId"
+          label="Id"
+          variant="outlined"
+          fullWidth
+          sx={{ my: 1 }}
+          value={amendUserId}
+          onChange={(e) => {
+            setAmendUserId(e.currentTarget.value);
+          }}
+        />
+        <TextField
+          id="userFirstName"
+          label="FirstName"
+          variant="outlined"
+          value={amendUserFirstName}
+          sx={{ my: 1, width: "100%", maxWidth: "48%" }}
+          onChange={(e) => {
+            setAmendUserFirstName(e.currentTarget.value);
+          }}
+        />
+        <TextField
+          id="userLastName"
+          label="LastName"
+          variant="outlined"
+          value={amendUserLastName}
+          sx={{ ml: "4%", my: 1, width: "100%", maxWidth: "48%" }}
+          onChange={(e) => {
+            setAmendUserLastName(e.currentTarget.value);
+          }}
+        />
+        <TextField
+          id="userNickName"
+          label="NickName"
+          variant="outlined"
+          fullWidth
+          sx={{ my: 1 }}
+          value={amendUserNickName}
+          onChange={(e) => {
+            setAmendUserNickName(e.currentTarget.value);
+          }}
+        />
+        <TextField
+          id="userEmail"
+          label="Email"
+          variant="outlined"
+          fullWidth
+          sx={{ my: 1 }}
+          value={amendUserEmail}
+          onChange={(e) => {
+            setAmendUserEmail(e.currentTarget.value);
+          }}
+        />
+        <Divider sx={{ mb: 1, mt: 1 }} />
+        <TextField
+          id="userIntroduction"
+          label="Introduction"
+          multiline
+          rows={4}
+          fullWidth
+          sx={{ my: 1 }}
+          value={amendUserIntroduction}
+          onChange={(e) => {
+            setAmendUserIntroduction(e.currentTarget.value);
+          }}
+        />
+        <Box display="flex" justifyContent="flex-end" sx={{ mt: 1 }}>
+          <Button variant="contained">Amend</Button>
+        </Box>
+      </Box>
     </>
   );
 };
