fea3fca732c88de077f4d8161c73d350290c2b49
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2024-02-01 17:31:10 +0900

    Feat: 유저 회원가입 api 구현 (백엔드)

--- Pizza_backend/src/main/java/com/example/Pizza_backend/controller/UserController.java
+++ Pizza_backend/src/main/java/com/example/Pizza_backend/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.Pizza_backend.controller;
 
-import com.example.Pizza_backend.dto.UserDto;
+import com.example.Pizza_backend.dto.UserCreateDto;
+import com.example.Pizza_backend.dto.UserLoginDto;
 import com.example.Pizza_backend.entity.User;
 import com.example.Pizza_backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +20,36 @@ public class UserController {
     private final UserRepository userRepository;
 
     @PostMapping("/api/user/create")
-    public ResponseEntity<User> createUser(@RequestBody UserDto userDto) {
-        System.out.println(userDto.toString());
+    public ResponseEntity<String> createUser(@RequestBody UserCreateDto userCreateDto) {
         User user = new User();
-        user.setUserId(userDto.getId());
-        user.setPassword(userDto.getPassword());
-        user.setEmail(userDto.getEmail());
-        user.setNickname(userDto.getNickname());
-        user.setFirstName(userDto.getFirstName());
-        user.setLastName(userDto.getLastName());
-        user.setIntroduction(userDto.getIntroduction());
+        user.setUserId(userCreateDto.getId());
+        user.setPassword(userCreateDto.getPassword());
+        user.setEmail(userCreateDto.getEmail());
+        user.setNickname(userCreateDto.getNickname());
+        user.setFirstName(userCreateDto.getFirstName());
+        user.setLastName(userCreateDto.getLastName());
+        user.setIntroduction(userCreateDto.getIntroduction());
 
-        userRepository.save(user);
+        try {
+            userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 존재하는 id 입니다.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @PostMapping("/api/login")
+    public ResponseEntity<User> loginUser(@RequestBody UserLoginDto userLoginDto) {
+
+        User user = new User();
+        user.setUserId(userLoginDto.getId());
+
+        User byUserId = userRepository.findByUserId(userLoginDto.getId());
+
+        System.out.println(byUserId.getEmail());
+
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 }
--- Pizza_backend/src/main/java/com/example/Pizza_backend/entity/User.java
+++ Pizza_backend/src/main/java/com/example/Pizza_backend/entity/User.java
@@ -1,19 +1,20 @@
 package com.example.Pizza_backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = "userId")
+})
 public class User {
 
     @Id @GeneratedValue
     private Long id;
 
-    private String userId;
+    private String userId;  // unique
     private String password;
     private String firstName;
     private String lastName;
